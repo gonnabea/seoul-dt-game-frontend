84d8feb386cf140871e9b715f9c2664a18a8d55d
Jiwon <winning1993@naver.com>
2022-07-09 02:15:21 +0900

    [ADD] Cesium.js + Vue.js 환경 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <OLCesium />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OLCesium from "@/components/OLCesium.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    OLCesium
   }
 }
 </script>
---
+++ src/components/OLCesium.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="cesiumContainer">
+    
+  </div>
+</template>
+
+<script>
+// import OLCesium from 'olcs/OLCesium.js';
+// import {View} from "ol";
+// import Map from 'ol/Map.js';
+// import OSM from 'ol/source/OSM';
+// import TileLayer from 'ol/layer/Tile';
+import * as Cesium from 'cesium';
+
+export default {
+  name: 'OLCesium',
+  props: {
+  
+  },
+  mounted() {
+//     const ol2dMap = new Map({
+//   layers: [
+    
+//     new TileLayer({
+//       source: new OSM(),
+//     }),
+//   ],
+//   target: 'map',
+//   view: new View({
+//     center: [0, 0],
+//     zoom: 2,
+//   }),
+// });
+
+
+//     const ol3d = new OLCesium({map: ol2dMap}); // ol2dMap is the ol.Map instance
+//     ol3d.setEnabled(true);
+//     console.log(ol3d)
+
+//     const scene = ol3d.getCesiumScene();
+
+//     console.log(scene)
+
+
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ZjJkMDA4NS0zYmQ2LTQ2NmItOWNjYy03Y2IyMTAwZWNlY2MiLCJpZCI6MTAwNTc3LCJpYXQiOjE2NTcyOTk1NDV9.WcA5OYJ1Cj3S0J30DR6Spb19hx-1bsF3ptiNZDEqKEU';
+
+const worldTerrain = Cesium.createWorldTerrain({
+  requestWaterMask: true,
+  requestVertexNormals: true,
+});
+
+const viewer = new Cesium.Viewer("cesiumContainer", {
+  terrainProvider: worldTerrain,
+});
+
+// set lighting to true
+viewer.scene.globe.enableLighting = true;
+
+
+var globe = viewer.scene.globe;
+globe.enableLighting = true;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
